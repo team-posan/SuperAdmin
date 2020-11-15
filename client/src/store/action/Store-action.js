d08209fd7ccdbcef65088471f871880e6b8b29b6
midham14 <mohammadidham14@gmail.com>
2020-11-15 20:11:02 +0700

    CRUD Store

--- client/src/store/action/Store-action.js
+++ client/src/store/action/Store-action.js
@@ -1,28 +1,82 @@
-import { FETCH_STORE } from ".";
+import axios from 'axios'
 
-export const setLogin = (payload) => {
-  return {
-    type: FETCH_STORE,
-    payload,
-  };
-};
 
-export const fetchdDataStore = () => {
-  return (dispatch) => {
-    fetch(`http://localhost:5000/store`)
-      .then((res) => res.json())
-      .then((result) => {
-        dispatch(setStore(result));
-      });
-  };
-};
+const baseUrlStore = 'http://localhost:5000/store'
 
-export const fetchdDataStore = () => {
-  return (dispatch) => {
-    fetch(`http://localhost:5000/store`)
-      .then((res) => res.json())
-      .then((result) => {
-        dispatch(setStore(result));
-      });
-  };
-};
+
+
+export const fetchStore=()=>{
+  return dispatch =>{
+    axios.get(baseUrlStore)
+      .then(({data})=>{
+        dispatch({type:'FETCH_STORE', payload:data})
+      })
+      .catch(err=>{
+        console.log(err)
+      })
+  }
+}
+
+export const addStore=(dataAdd)=>{
+  return dispatch =>{
+    const { store_name, store_address } = dataAdd
+    axios.post(baseUrlStore,{store_name,store_address},{
+      headers:{
+        access_token:localStorage.getItem('access_token')
+      }
+    })
+    .then(({data})=>{
+      dispatch({
+        type:'ADD_STORE',
+        payload:{store_name,store_address,id:data.id}
+      })
+    })
+    .catch(err=>{
+      console.log(err)
+    })
+  }
+}
+
+
+export const editStore=(dataEdit)=>{
+  return dispatch =>{
+    const { store_name, store_address,id } = dataEdit
+    axios.patch(baseUrlStore+'/'+id,{store_name,store_address},{
+      headers:{
+        access_token:localStorage.getItem('access_token')
+      }
+    })
+    .then(result=>{
+      console.log(result)
+      dispatch({
+        type:'EDIT_STORE',
+        payload:{store_name,store_address,id}
+      })
+    })
+    .catch(err=>{
+      console.log(err)
+    })
+  }
+}
+
+
+export const deleteStore=(dataDelete)=>{
+  return dispatch =>{
+    axios.delete(baseUrlStore+'/'+dataDelete.id,{
+      headers:{
+        access_token:localStorage.getItem('access_token')
+      }
+    })
+     .then((result)=>{
+       console.log(result)
+       dispatch({
+         type:'DELETE_STORE',
+         payload : dataDelete
+       })
+     })
+     .catch(err=>{
+       console.log(err)
+     })
+
+  }
+}
